23296d938cee055a7ced6c26602bf6d1c0676c8d
Chad <mcadamschad@outlook.com>
2019-02-12 09:18:10 -0800

    fixed part of first set for lab2, changed input and output file names to better understand what they do

--- labs/lab2/lab2.cpp
+++ labs/lab2/lab2.cpp
@@ -1,24 +1,44 @@
 #include <fstream>
 #include <string>
 #include <iostream>
+#include <vector>
 
-std::string getFirstSet(std::string sI)
+bool isTerminal(std::string s, std::vector<std::string>terminals)
+{
+    for(int i=0; i<terminals.size(); i++)
+    {
+        if(terminals[i] == s)
+        {
+            return true;
+        }
+    }
+    return false;
+}
+
+std::string getFirstSet(std::string sI, std::vector<std::string>terminals)
 {
     std::string sO = "";
     for(int i=0; i<sI.length(); i++)
     {
-        if(sI.substr(i, 2) == "->") sO += sI.substr(i+2, 1);
-        else if(sI.substr(i, 1) == "|") sO += sI.substr(i+2, 1);
+        if(sI.substr(i, 2) == "->")
+        {
+            if(isTerminal(sI.substr(i+2, 1), terminals) || sI.substr(i+2, 1) == "e") sO += sI.substr(i+2, 1); 
+        }
+        if(sI.substr(i, 1) == "|") 
+        {
+            if(isTerminal(sI.substr(i+1, 1), terminals) || sI.substr(i+2, 1) == "e") sO += sI.substr(i+1, 1);
+        }
     }
     return sO;
 }
 
 int main () {
   std::string line;
-  std::ifstream inGram("g417");
-  std::ofstream outGram("sets.out");
+  std::ifstream inGram("g417.in");
+  std::ofstream outGram("sets_g417.out");
   int lineNum = 1;
   int terminals = 0;
+  std::vector<std::string>vTerminals;
   if(inGram.is_open())
   {
     		while (std::getline(inGram, line))
@@ -27,11 +47,11 @@ int main () {
                 {
                     if(terminals == 0)
                     {
-                        
+                        vTerminals.push_back(line.substr(0, 1));
                     }
                     else if(terminals == 1)
                     {
-                        outGram << getFirstSet(line);
+                        outGram << getFirstSet(line, vTerminals);
                     }
                     else
                     {
