ea98e6274f23ecd8eb59ba19d7792096812efb1b
Chad <mcadamschad@outlook.com>
2019-02-12 01:28:33 -0800

    added first set to string, need to add follow set to string (vector is also an option)

---
+++ labs/lab2/lab2.cpp
@@ -0,0 +1,50 @@
+#include <fstream>
+#include <string>
+#include <iostream>
+
+std::string getFirstSet(std::string sI)
+{
+    std::string sO = "";
+    for(int i=0; i<sI.length(); i++)
+    {
+        if(sI.substr(i, 2) == "->") sO += sI.substr(i+2, 1);
+        else if(sI.substr(i, 1) == "|") sO += sI.substr(i+2, 1);
+    }
+    return sO;
+}
+
+int main () {
+  std::string line;
+  std::ifstream inGram("g417");
+  std::ofstream outGram("sets.out");
+  int lineNum = 1;
+  int terminals = 0;
+  if(inGram.is_open())
+  {
+    		while (std::getline(inGram, line))
+    		{
+                if(outGram.is_open())
+                {
+                    if(terminals == 0)
+                    {
+                        
+                    }
+                    else if(terminals == 1)
+                    {
+                        std::cout << getFirstSet(line);
+                    }
+                    else
+                    {
+                        //return 0;
+                    }
+                    if(line == "$")
+                    {
+                        terminals++;
+                    }
+                    lineNum++;
+                }
+  	        }
+  }
+  inGram.close();
+  return 0;
+}
